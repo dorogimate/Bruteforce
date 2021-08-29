dd958c68e26d701d3068f854ab869943847e3ad4
dorogimate <dorogimate6@gmail.com>
2021-08-29 10:26:17 +0200

    add environment variables for database connection, since cannot be used

--- database_common.js
+++ database_common.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
+// Why does not it reach environmental variables -> cursor over process.env.MYSQL_USERNAME can see my variable, but
+// with console log it says undefined
+let username = process.env.MYSQL_USERNAME;
+console.log(username);
+
+// Now has to set manually your own connection variables -> the above code will be implemented if working
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'Dorogi',
@@ -7,7 +14,7 @@ const connection = mysql.createConnection({
     database: 'Nodejs_test'
 });
 
-
+// How to return the value of the query and make possible to call the function in main.js -> then use its return value
 function firstQuery () {
     connection.query("SELECT * FROM users", function (err, result, fields) {
         if (err) {return err}
